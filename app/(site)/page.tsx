b18fe73aca53f0ed934429a5d917c4b301217cb5
RajAdroja <rajdineshbhai.adroja@sjsu.edu>
2025-01-06 02:31:42 -0800

    adding auth form

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,17 +1,38 @@
+import Image from 'next/image';
+import AuthForm from './components/AuthForm';
 export default function Home() {
     return (
       <div
-        className="
-        flex
-        min-h-full
-        flex-col
-        justify-center
-        py-12
-        sm:px-6
-        lg:px-8
-        bg-gray-100
+          className="
+          flex
+          min-h-full
+          flex-col
+          justify-center
+          py-12
+          sm:px-6
+          lg:px-8
+          bg-gray-100
         ">
-       
+          <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <Image
+              className="mx-auto w-auto"
+              src="/images/messenger-logo.png"
+              alt="logo"
+              width="48"
+              height="48"
+            />
+            <h2 className="
+              mt-6 
+              text-3xl 
+              font-bold 
+              tracking-tight 
+              text-center 
+              text-gray-900">
+              Sign in to your account
+            </h2>
+
+          </div>
+        <AuthForm />
       </div>
     );
   }
