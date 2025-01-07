7d474b30c3c278f2c16daff5de2ddf3b48fac180
RajAdroja <rajdineshbhai.adroja@sjsu.edu>
2025-01-06 16:21:07 -0800

    Adding Login/Register UI

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,14 +1,14 @@
 'use client';
-
+import {BsGithub, BsGoogle} from 'react-icons/bs';
 import { useCallback, useState } from 'react';
 import { 
     FieldValues, 
     useForm , 
     SubmitHandler 
 } from 'react-hook-form';
-import Input  from "@/app/components/inputs/input";
-
-
+import Input  from "@/app/components/Inputs/Input";
+import Button from "@/app/components/Button";
+import AuthSocialbutton from "./AuthSocialButton";
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
@@ -80,6 +80,7 @@ const AuthForm = () => {
                         label="Name" 
                         register={register} 
                         errors={errors}
+                        disabled={isLoading}
                     />
                     )}
                     <Input 
@@ -88,6 +89,7 @@ const AuthForm = () => {
                         type ="email"
                         register={register} 
                         errors={errors}
+                        disabled={isLoading}
                     />
                     <Input 
                         id="password" 
@@ -95,11 +97,105 @@ const AuthForm = () => {
                         type="password"
                         register={register} 
                         errors={errors}
+                        disabled={isLoading}
                     />
+                    <div>
+                        <Button
+                            disabled={isLoading}
+                            fullWidth
+                            type="submit"
+                        >
+                            {variant === 'LOGIN' ? 'Sign In' : 'Register'}
+                        </Button>
+                    </div>
                 </form>
+                
+                <div className="mt-6">
+                    {/* <div className="relative">
+                        <div
+                            className="
+                                absolute
+                                inset-0
+                                flex
+                                items-center
+                            "
+                        >
+                            <div 
+                            className="
+                                w-full 
+                                border-t 
+                                border-gray-300"
+                            />
+                        </div>
+                        <div
+                        className="
+                            relative
+                            flex
+                            justify-center
+                            text-sm
+                            "
+                        >
+                            <span className="
+                                bg-whihte 
+                                px-2
+                                text-gray-500">
+                                    Or Continue with
+                            </span>
+                        </div>
+                    </div> */}
+                    <div className="relative flex items-center">
+                        <div className="flex-grow border-t border-gray-300"></div>
+                            <span
+                                className="
+                                    mx-4
+                                    bg-white 
+                                    text-gray-500 
+                                    text-sm 
+                                    whitespace-nowrap
+                                ">
+                                Or Continue with
+                            </span>
+                        <div className="flex-grow border-t border-gray-300"></div>
+                    </div>
+
+                    
+
+
+                    <div className="mt-6 flex gap-2">
+                        <AuthSocialbutton
+                            icon={BsGithub}
+                            onClick={() => socialAction('github')}
+                        />
+                        <AuthSocialbutton
+                            icon={BsGoogle}
+                            onClick={() => socialAction('google')}
+                        />
+
+                    </div>
+                </div>
+                <div className="
+                    flex
+                    gap-2
+                    justify-center
+                    text-sm
+                    mt-6
+                    px-2
+                    text-gray-500
+                ">
+                    <div>
+                        {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'}
+                    </div>
+
+                    <div 
+                        onClick={toggleVariant}
+                        className="underline cursor-pointer"
+                    >
+                        {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default AuthForm;
