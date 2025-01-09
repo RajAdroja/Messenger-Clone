06d03cf3df2275e85c4a10279fed0f9bc1e36033
RajAdroja <rajdineshbhai.adroja@sjsu.edu>
2025-01-08 21:51:07 -0800

    updating AuthContext

--- app/context/AuthContext.tsx
+++ app/context/AuthContext.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider } from "next-auth/react";
 
-interface AuthContextProps{
-    children: React.ReactNode;
+interface AuthContextProps {
+  children: React.ReactNode;
 }
 
 export default function AuthContext({
-    children
-}: AuthContextProps){
-    return <SessionProvider> {children} </SessionProvider>
+  children
+}: AuthContextProps) {
+  return <SessionProvider>{children}</SessionProvider>
 }
