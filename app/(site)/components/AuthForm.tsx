e1615499d59eed84f03cef351dad2eb3439c80ed
Raj Adroja <rajadroja015@gmail.com>
2025-10-13 21:20:18 -0700

    adding border below input fields and button

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -99,6 +99,20 @@ const AuthForm = () => {
             </Button>
           </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div
+              className="
+            absolute
+            inset-0
+            flex
+            items-center
+            "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
