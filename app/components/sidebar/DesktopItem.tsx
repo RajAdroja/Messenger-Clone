7f3050cc8efba5252a10d995df9d3fd69ac22ae8
Raj Adroja <rajadroja015@gmail.com>
2025-10-16 17:55:43 -0700

    add Desktop sidebar component and related hooks

---
+++ app/components/sidebar/DesktopItem.tsx
@@ -0,0 +1,55 @@
+'use client';
+import clsx from 'clsx';
+import Link from 'next/link';
+
+interface DesktopItemProps {
+  href: string;
+  label: string;
+  icon: any;
+  active?: boolean;
+  onClick?: () => void;
+}
+
+const DesktopItem: React.FC<DesktopItemProps> = ({
+  href,
+  label,
+  icon: Icon,
+  active,
+  onClick,
+}) => {
+  const handleCLick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+
+  return (
+    <li onClick={handleCLick}>
+      <Link
+        href={href}
+        className={clsx(
+          `
+        group
+        flex
+        gap-x-3
+        rounded-md
+        p-3
+        text-sm
+        leading-6
+        font-semibold
+        text-gray-500
+        hover:text-black
+        hover:bg-gray-100
+        hover:cursor-pointer
+      `,
+          active && 'bg-gray-100 text-black'
+        )}
+      >
+        <Icon size={28} className="h-6 w-6 shrink-0" />
+        <span className="sr-only">{label}</span>
+      </Link>
+    </li>
+  );
+};
+
+export default DesktopItem;
